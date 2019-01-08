c9af684121196c081aa719d67cd47902f7daae89
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-08 18:59:21 +0700

    Variables

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+let name = "Adi"
+console.log(name)
+
+//Cannot be a reserved keyword (if else dll)
+//Shoulbe meaningful
+//Cannot Start With Number
+//Cannot contain a space or hyphen (-)
+
+//Are Case-Sensitive
+let firstName = 'Adi', lastName;
+
+
+
