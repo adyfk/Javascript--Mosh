e7186a9d873fc2e79619569e1230ad693c8dd65d
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-08 19:27:59 +0700

    Operator

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function getName(params) {
-    console.log(params)
-}
+let x = 5, y = 2
 
-getName('Adi')
+console.log(x + y)
+console.log(x - y)
+console.log(x % y)
+console.log(x / y)
+console.log(x * y)
+console.log(x ** 3)     //exponent (Pangkat)
+
+//increment (inverse)
+
+
+// Ternary Operator
+let x = 10 > 2 ? 'yes' : 'No'
+
+
+//Logic with Non Bolean
+
+console.log(false || 'Adi')
+//Adi
+
+
