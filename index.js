b440c030a7fb0347a58c8836a111c78a9788cfac
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 09:11:12 +0700

    Dynamic Nature Object

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 const circle = {
     radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function () {
-        console.log('draw')
-    }
 }
-//Factory FUnction
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log('draw')
-        }
-    }
-}
-const circle1 = createCircle(1)
-console.log(circle1)
-const circle2 = createCircle(2)
-console.log(circle2)
+circle.color = 'Yellow'
+circle.draw = function () { }
 
-//Constructor Function
-//Pascal Notation
+console.log(circle)
 
-function Circle(radius) {
-    this.radius = radius
-    this.draw = () => {
-        console.log('draw')
-    }
-}
-const circle = new Circle(3);
+delete circle.color
+
+console.log(circle)
