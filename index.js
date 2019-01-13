81785b31a48471202a0ede56f735a0cc35144d4b
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 17:02:48 +0700

    Moving an Element

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const numbers = [1, 2, 3, 4, 5, 1, 1]
 
-const output = except(numbers, [1, 2])
+const output = move(numbers, 0, 3)
 
 console.log(output)
 
-function except(array, excluded) {
-    const output = []
-    for (let element of array)
-        if (!excluded.includes(element))
-            output.push(element)
+function move(array, index, offset) {
+    const output = [...array]
+    const element = output.splice(index, 1)[0]
+    output.splice(index + offset, 0, element)
     return output
 }
 
