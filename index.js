d9fcc687320d652790f2240505d9d2b267d44c70
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 10:31:49 +0700

    Add , Delete , Find Array

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const data = 'Adi'
-const message = 'This is My\n\'first\' Message ' + data
-console.log(message)
+const numbers = [3, 4, 5]
+//Add End
+numbers.push(7, 8, 9)
+//Add Begining
+numbers.unshift(-1, 0, 1)
+//replace and delete
+numbers.splice(2, 1, 'a', 'b')
+console.log(numbers)
 
-const another =
-    `This is My ${4 + 2}
-'First' Text Lo ${data}`
-console.log(another)
+const nomer = [1, 2, 3, 4, 5, 6, 1, 7]
+console.log(nomer.indexOf('1'))
+console.log(nomer.lastIndexOf(1))
+
+console.log(nomer.indexOf(1) != -1)//mean is Exist or not
+console.log(nomer.includes(9)) //mean is Exist or not
+
+
+//FInd a Object in Array
+const course = [
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' }
+]
+const cek = course.find(function (element) {
+    return element.name === 'a'
+})
+
+console.log('Cek', cek)
+//Return 0 if Exist
+const cik = course.findIndex(function (element) {
+    return element.name === 'aaa'
+})
+
+console.log('Cek', cik)
