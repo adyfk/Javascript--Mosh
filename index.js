dad61f6070a1a9ca2e88c1113eff68ae6cc4ce1f
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 09:07:03 +0700

    Constructor Function

--- index.js
+++ index.js
@@ -22,3 +22,14 @@ const circle1 = createCircle(1)
 console.log(circle1)
 const circle2 = createCircle(2)
 console.log(circle2)
+
+//Constructor Function
+//Pascal Notation
+
+function Circle(radius) {
+    this.radius = radius
+    this.draw = () => {
+        console.log('draw')
+    }
+}
+const circle = new Circle(3);
