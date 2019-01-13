475698173b0565e589e0316570240f46703ecbe2
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 16:03:42 +0700

    Mapping an Array

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const numbers = [-1, 2, 3, -4]
+const numbers = [-1, 2, 3, 4]
 
-numbers.filter((val) => val > 0)
-console.log(numbers.filter(val => val >= 0))
+const filtered = numbers.filter((val) => val >= 0)
+console.log(filtered)
+
+const itemsToObj = filtered.map(n => ({ value: n }))
+console.log(itemsToObj)
+
+const items = filtered.map(n => '<li>' + n + '</li>')
+const html = '<ul>' + items.join('') + '</ul>'
+console.log(html)
+
+
+//How to Make it Simple
+const items2 = numbers
+    .filter(val => val >= 0)
+    .map(n => ({ value: n }))
+    .filter(obj => obj.value > 2)
+    .map(obj => obj.value)
+console.log(items2)
+
