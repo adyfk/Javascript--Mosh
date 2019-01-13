8ef1e18df2081c205c3132fac98b94a2fddc4c50
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 16:37:37 +0700

    Reducing an Array

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-const numbers = [-1, 2, 3, 4]
+const numbers = [-1, 2, -1, 3, 4]
 
-const filtered = numbers.filter((val) => val >= 0)
-console.log(filtered)
+let sum = 0
+for (let n of numbers)
+    sum += n
+console.log(sum)
 
-const itemsToObj = filtered.map(n => ({ value: n }))
-console.log(itemsToObj)
+sum = numbers.reduce((accumulator, currentvalue) => accumulator + currentvalue)   //optional params last for initial value of accumulator 
 
-const items = filtered.map(n => '<li>' + n + '</li>')
-const html = '<ul>' + items.join('') + '</ul>'
-console.log(html)
-
-
-//How to Make it Simple
-const items2 = numbers
-    .filter(val => val >= 0)
-    .map(n => ({ value: n }))
-    .filter(obj => obj.value > 2)
-    .map(obj => obj.value)
-console.log(items2)
-
+console.log(sum)
