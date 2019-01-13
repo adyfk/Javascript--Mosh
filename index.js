193fd61c5847cc00bc51a469f2a7e98175d86a08
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 14:27:44 +0700

    Testting Array Or Set value Some Array or All array

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-const numbers = [1, 2, 3, 4, 2, 1]
+const numbers = [-1, -2, -3, -4]
+
+const allPositive = numbers.every((val) => val >= 0)    //every
+const allPositive2 = numbers.some((val) => val >= 0)    //atLeastOne (salah satu)
 console.log(numbers)
-console.log(numbers.sort())
-console.log(numbers.reverse())
-
-
-//example when Object
-
-const course = [
-    { id: 2, name: 'Javascript' },
-    { id: 1, name: 'ANode JS' },
-    { id: 3, name: 'BNode JS' },
-    { id: 4, name: 'ZNode JS' },
-    { id: 5, name: 'PNode JS' }
-]
-course.sort((first, second) => {
-    if (first.name < second.name) return -1
-    else if (first.name > second.name) return 1
-    return 0
-})
-console.log(course)
+console.log(allPositive)
+console.log(allPositive2)
