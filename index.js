a345ec37becda67aaab46d01b14d76e051a94bfc
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-13 14:31:37 +0700

    Filter Array

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const numbers = [-1, -2, -3, -4]
+const numbers = [-1, 2, 3, -4]
 
-const allPositive = numbers.every((val) => val >= 0)    //every
-const allPositive2 = numbers.some((val) => val >= 0)    //atLeastOne (salah satu)
-console.log(numbers)
-console.log(allPositive)
-console.log(allPositive2)
+numbers.filter((val) => val > 0)
+console.log(numbers.filter(val => val >= 0))
