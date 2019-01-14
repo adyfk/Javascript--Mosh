c98e1fdd7c06798d5d10c9ebd691b4093f4d7a0f
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-14 09:43:28 +0700

    Function , Argument , Thr Rest Operator , Default Parameter

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const movies = [
-    { title: 'a', year: 2018, rating: 4.5 },
-    { title: 'b', year: 2018, rating: 5 },
-    { title: 'c', year: 2018, rating: 5 },
-    { title: 'd', year: 2019, rating: 4.6 },
-]
+//Function Declaration
+function walk() {
+    console.log('run')
+}
 
-const title = movies
-    .filter(m => m.year === 2018 && m.rating >= 4.5)
-    .sort((a, b) => a.rating - b.rating)
-    .reverse()
-    .map(m => m.title)
+//Function Expression
+let run = function () {
+    console.log('run')
+}
 
-console.log(title)
+//argument
+function sum() {
+    console.log(arguments)
+    let total = 0
+    for (let value of arguments)
+        total += value
+    return total
+}
+console.log(sum(5, 4, 3, 2))
+
+// The Best Operator
+function sum2(...args) {
+    return args.reduce((a, b) => a + b)
+}
+console.log(sum2(5, 4, 3, 2))
+
+// The Best Operator
+function sum3(discount, ...args) {
+    const total = args.reduce((a, b) => a + b)
+    return total * (1 - discount)
+}
+console.log('Discount', sum3(0.1, 20, 30))
+
+//Default Paramter
+function interest(principal, rate = 3.5, years = 5) {
+    return principal * rate / 100 * years
+}
+console.log('Interest', interest(10000, undefined, 3))
